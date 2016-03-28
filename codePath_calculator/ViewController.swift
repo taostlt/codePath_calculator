905e4398318571c0e99ae88c750fe652118b832d
taostlt <corazon@corazons-mbp.attlocal.net>
2016-03-28 15:00:42 -0700

    add initial commit

---
+++ codePath_calculator/ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  codePath_calculator
+//
+//  Created by Corazon on 3/15/16.
+//  Copyright © 2016 Corazon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    @IBOutlet weak var billField: UITextField!
+
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        
+        
+        var billAmount = NSString(string: billField.text!).doubleValue
+        
+        
+        
+        var tipPercentages = [0.30, 0.25, 0.20, 0.15]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        var tip = billAmount*tipPercentage
+        
+        var total = billAmount + tip
+        
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+//        
+//        tipLabel.text = String(format: "$%.2f", tip)
+//        totalLabel.text = String(format: "$%.2f", tip)
+    }
+
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+   
+}
+
---
+++ codePath_calculatorTests/codePath_calculatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  codePath_calculatorTests.swift
+//  codePath_calculatorTests
+//
+//  Created by Corazon on 3/15/16.
+//  Copyright © 2016 Corazon. All rights reserved.
+//
+
+import XCTest
+@testable import codePath_calculator
+
+class codePath_calculatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
